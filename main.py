36c800be7497f3f279badb57b8c3c8ef008ded3c
sagarsaha03 <85450054+sagarsaha03@users.noreply.github.com>
2025-07-30 11:38:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-# main.py
 import asyncio
+import logging
 from aiogram import Bot, Dispatcher
-from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
-
-from config import BOT_TOKEN
-from router_admin import router as admin_router
-from router_forwarder import router as forwarder_router
-from handlers.help import router as help_router
+from config import Config, setup_db
+from router_admin import admin_router
+from router_forwarder import forward_router
+from handlers.help import help_router
 
 async def main():
-    bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+    setup_db()
+    bot = Bot(token="8016591770:AAFXBKlZS0HRlm1ThGhOKXmNymAHHvnNTDE")
     dp = Dispatcher(storage=MemoryStorage())
-
-    # Register routers
+    
     dp.include_router(admin_router)
-    dp.include_router(forwarder_router)
+    dp.include_router(forward_router)
     dp.include_router(help_router)
-
-    print("Bot is starting...")
+    
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
